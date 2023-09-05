338e60fd7bd7f91008c7a975fac699de38ac2c1b
Ssanika13 <144125409+Ssanika13@users.noreply.github.com>
2023-09-05 18:36:08 +0530

    first

---
+++ decom/signup/serializers.py
@@ -0,0 +1,8 @@
+
+from rest_framework import serializers
+from .models import student
+
+class studentserializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=student
+        fields=['id', 'name', 'roll', 'city',]
